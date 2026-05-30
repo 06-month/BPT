8b9d125f1302d72fa7521412da2a577174062f74
jinjeonz <20237134@edu.hanbat.ac.kr>
2026-05-31 02:07:59 +0900

    chore: 미사용 Firebase import 제거

--- bpt/lib/services/workout_records_service.dart
+++ bpt/lib/services/workout_records_service.dart
@@ -1,5 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../models/workout_record_model.dart';
